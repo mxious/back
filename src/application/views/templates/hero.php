e556092e19d65f9ce55fcf33baad0581de843489
Sergio <sergio@alphasquare.us>
2015-03-20 13:32:19 -0400

    src: remove unused files and add CSS

---
+++ src/application/views/templates/hero.php
@@ -0,0 +1,17 @@
+<div id="index-banner"  style="z-index: -1;top: 0;" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container main-container">
+        <br><br>
+        <h1 class="header center">where the dreamers go</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">Share albums, music videos, and build the ultimate music collection.</h5>
+          <a href="register" id="download-button" class="btn-large pull-right waves-effect waves-light yellow darken-4">Get Started</a>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="<?php echo SITE_IMG; ?>"></div>
+  </div>
+  <div>
+  <br>
---
+++ src/assets/js/dash.js
@@ -0,0 +1,68 @@
+/* 
+ * Mxious JS Engine Dash
+ * (copy) Mxious, The Alphasquare Group,
+ * (copy [..]) and Alphasquare Open Source
+*/
+
+var Dash = Dash || {
+
+	feed_container: '#layout',
+	// offset is 5 because php loads 5
+	offset: 5,
+
+	init: function () {
+		this.bind();
+	},	
+
+	feed: {
+		init: function (elem, type) {
+			Dash.feed_container = elem;
+
+			$(window).scroll(function(){
+				if ($(window).scrollTop() == $(document).height() - $(window).height()){
+					Dash.feed.ajax.load_more(type);
+				}
+			});
+		},
+		ajax: {
+			load_more: function (type) {
+				$.getJSON('post/load_more', {offset: Dash.offset, type: type}).done(function (data) {
+					elem = $(Dash.feed_container);
+					offset = Dash.offset + data.count;
+					Dash.offset = offset;
+					if (Mxious.mode == "development") {
+						debug = {
+							offset: offset,
+							container: Dash.feed_container
+						};
+						console.log("Mxious: loading more data:" + debug.offset + "," + debug.container);
+					}
+
+					var elem = jQuery(data.html);
+	                $(Dash.feed_container).append(elem).imagesLoaded( function() { $(Dash.feed_container).masonry('appended', elem, true); });
+				});
+			}
+		},
+	},
+
+	post: {
+
+	},
+
+	comments: {
+
+	},
+
+	bind: function () {
+
+	},
+
+	load_masonry: function () {
+	   $(Dash.feed_container).masonry({
+	      "itemSelector": '.item-masonry',
+	      "columnWidth": 250,
+	      "isFitWidth": true
+	   });
+	}
+
+}
