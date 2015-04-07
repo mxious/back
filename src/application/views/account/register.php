c44737497278b9701e679c5c5d6847607672fe98
Sergio <sergio@alphasquare.us>
2015-04-07 13:18:22 -0400

    Added register view

--- src/application/views/account/register.php
+++ src/application/views/account/register.php
@@ -1,43 +1,55 @@
 <?php show_form_errors($errors); ?>
 
-<div class="row">
-	<div class="col-lg-6 col-md-8 col-sm-10 col-centered">
-		<div class="page-header">
-			<h1>Register</h1>
-		</div>
+<div class="container login-panel row">
+ <div class="col s6 offset-s3">
+   <div class="card">
+    <div class="card-content">
+       <span class="card-title grey-text text-darken-4">Register </span>
+        <p>
+            <form action="" method="post">
+              <input type="hidden" name="next" value="<?=htmlentities($this->input->get('next'))?>" >
 
-		<?php // $this->load->view('account/oauth_buttons'); ?>
-		<?php
-			if ($recaptchaerror == true) {
+              	<div class="row">
+              	<div class="input-field col s12">
+              		<input type="text" name="username" id="username" placeholder="Username" class="form-control" value="<?=set_value('username')?>" />
+              	</div>
+              	</div>
 
-				echo '<br><div class="alert alert-info">The recaptcha was incorrect. Please try again.</div></br>';
+              	<div class="row">
+              	<div class="input-field col s12">
+              		<input type="email" name="email" id="email" placeholder="Email" class="form-control" value="<?=set_value('email')?>" />
+              	</div>
+              	</div>
 
-			}
-		?>
-		<form action="" method="post">
-			<div class="form-group">
-				<input type="text" name="username" id="username" placeholder="Username" class="form-control" value="<?=set_value('username')?>" />
-			</div>
-			<div class="form-group">
-				<input type="email" name="email" id="email" placeholder="Email" class="form-control" value="<?=set_value('email')?>" />
-			</div>
-			<div class="form-group">
-				<input type="password" name="password" id="password" placeholder="Password" class="form-control" />
-			</div>
-			<div class="form-group">
-				<input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" class="form-control" />
-			</div>
-			<p>By using Alphasquare, you agree with the
-				<a href="about/terms" target="_blank">terms of service</a>
-				and <a href="about/privacy" target="_blank">privacy policy</a>.</p>
-				<br>
-				 <?php echo $recaptcha_html; ?>
-				 <br>
-			<button name="submit" class="btn btn-primary" value="true">Register</button>
-		</form>
-		<br />
-		<div class="well text-center">
-			Already have an account? <a href="<?=base_url('login')?>">Sign in &raquo;</a>
-		</div>
-	</div>
+              	<div class="row">
+              	<div class="input-field col s12">
+              		<input type="password" name="password" id="password" placeholder="Password" class="form-control" />
+              	</div>
+              	</div>
+
+              	<div class="row">
+              	<div class="input-field col s12">
+              		<input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" class="form-control" />
+              	</div>
+              	</div>
+
+                <p>By using Alphasquare, you agree with the
+              	terms of service and privacy policy.</p>
+              	<br>
+              	 <?php echo $recaptcha_html; ?>
+              	 <br>
+
+            <button name="submit" class="btn yellow darken-4" value="true">Sign up</button>
+          
+            </form>
+          </p>
+          </div>
+           <div class="card-action">
+              <a href="<?=base_url('account/forgot_password')?>">Forgot your password?</a>
+              <a href='<?=base_url('account/register')?>'>Create an account</a>
+            </div>
+
+        </div>
+      </div>
+
 </div>
