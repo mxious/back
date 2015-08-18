4b30b49b24062c9daf4768e912fd01207216a5f0
Sergio <sergio@alphasquare.us>
2015-08-18 09:03:49 -0400

    [CORE] Created "Home" controller and erased scaffolding

--- fuel/app/classes/controller/home.php
+++ fuel/app/classes/controller/home.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace Controller;
+
+use \Controller_Template as Controller_Template;
+use \View as View;
+use \Helper as Helper;
+
+/**
+ * Home controller
+ * 
+ * @author  Sergio Diaz <sergio@alphasquare.us>
+ * @package  Controllers 
+ * @extends Controller_Template
+ */
+
+class Home extends Controller_Template 
+{
+
+	public function action_index()
+	{
+		if (Helper::is_loggedin()) {
+			\Response::redirect('/dashboard');
+		}
+		$this->template->content = View::forge('home/content');
+	}
+
+}
